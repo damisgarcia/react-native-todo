a15651b9ffe006e413b87deb8bad3f7efb62e5c5
Damis Garcia <damisgarcia@gmail.com>
2017-09-05 00:19:09 -0300

    Add New Rules for Todos

--- services/Models.js
+++ services/Models.js
@@ -13,49 +13,51 @@ export const User = {
 
 export const Todo = {
   all: (callback) => {
-    return Firebase.database().ref(`/users/${UID}/todos`).on("value", (snapshot) => callback(snapshot) )
+    return Firebase.database().ref(`/todos`).orderByChild(`members/${UID}`).equalTo(true).on("value", (snapshot) => callback(snapshot) )
   },
   get: async (key, callback) => {
-    return await Firebase.database().ref(`/users/${UID}/todos/${key}`).once("value").then(callback)
+    return await Firebase.database().ref(`/todos/${key}`).once("value").then(callback)
   },
   save: async (data) => {
     let updates = {}
     let todoKey = data.key || null
 
     if(!todoKey){
-      todoKey =  Firebase.database().ref(`/users/${UID}`).child('todos').push().key;
+      todoKey =  Firebase.database().ref(`/todos`).push().key;
+
+      data.members = {}
+      data.members[UID] = true
     }
 
-    updates[`/users/${UID}/todos/${todoKey}`] = _.omit(data, 'key') ;
+    updates[`/todos/${todoKey}`] = _.omit(data, 'key') ;
 
     return Firebase.database().ref().update(updates);
   },
   destroy: async(todoKey) =>{
-    console.log(todoKey)
-    return await Firebase.database().ref(`/users/${UID}/todos/`).child(todoKey).remove()
+    return await Firebase.database().ref(`/${UID}/todos/`).child(todoKey).remove()
   }
 }
 
 export const Task = {
   all: (todo, callback) => {
-    return Firebase.database().ref(`/users/${UID}/todos/${todo.key}/tasks`).on("value", (snapshot) => callback(snapshot) )
+    return Firebase.database().ref(`/todos/${todo.key}/tasks`).on("value", (snapshot) => callback(snapshot) )
   },
   get: async (todo, key, callback) => {
-    return await Firebase.database().ref(`/users/${UID}/todos/${todo.key}/tasks/${key}`).once("value").then(callback)
+    return await Firebase.database().ref(`/todos/${todo.key}/tasks/${key}`).once("value").then(callback)
   },
   save: async (todo, data) => {
     let updates = {}
     let taskKey = data.key || null
 
     if(!taskKey){
-      taskKey =  Firebase.database().ref(`/users/${UID}/todos/${todo.key}`).child('tasks').push().key;
+      taskKey =  Firebase.database().ref(`/todos/${todo.key}`).child('tasks').push().key;
     }
 
-    updates[`/users/${UID}/todos/${todo.key}/tasks/${taskKey}`] = _.omit(data, 'key') ;
+    updates[`/todos/${todo.key}/tasks/${taskKey}`] = _.omit(data, 'key') ;
 
     return Firebase.database().ref().update(updates);
   },
   destroy: async(todo, taskKey) =>{
-    return await Firebase.database().ref(`/users/${UID}/todos/${todo.key}/tasks`).child(taskKey).remove()
+    return await Firebase.database().ref(`/todos/${todo.key}/tasks`).child(taskKey).remove()
   }
 }
